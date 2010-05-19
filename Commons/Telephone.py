4201e9089c3e570b9e7bd363d075e9c4851f8d6c
redrooster <devnull@localhost>
2010-05-19 15:56:05 +0200

    Verbesserung des Telefon encodings.

--- Commons/Telephone.py
+++ Commons/Telephone.py
@@ -10,17 +10,20 @@ def encode_phonenumber(ctry_code, area_code, phone_number):
 def decode_phonenumber(phone_number):
     ctry_code = '' 
     area_code = ''
+    cut_at = -1
     
     # Get the "contry code" ---------------------------------------------------
     phone_number = phone_number.lstrip()
     if phone_number.startswith('00') or \
        phone_number.startswith('+'):
         cut_at = phone_number.find(' ')
-        ctry_code = phone_number[0:cut_at].replace('00', '+')
-        if not ctry_code.startswith('+'):
-            ctry_code = '+' + ctry_code
-        phone_number = phone_number[cut_at:len(phone_number)]
-        cut_at = None
+        
+        if cut_at <> -1:
+            ctry_code = phone_number[0:cut_at].replace('00', '+')
+            if not ctry_code.startswith('+'):
+                ctry_code = '+' + ctry_code
+            phone_number = phone_number[cut_at:len(phone_number)]
+            cut_at = -1
     
     # Get the "area code" -----------------------------------------------------
     phone_number = phone_number.lstrip()
@@ -32,12 +35,13 @@ def decode_phonenumber(phone_number):
         cut_at = phone_number.find(' ')
     elif phone_number.find('-') <> -1:
         cut_at = phone_number.find('-')
+    
         
-    if cut_at <>  None:
+    if cut_at <> -1:
         area_code = phone_number[0:cut_at].replace('(', '')
         phone_number = phone_number[cut_at:len(phone_number)]
         
-    if not area_code.startswith('0'):
+    if not area_code.startswith('0') and area_code <> '':
         area_code = '0' + area_code
         
     # Get the "phone number" --------------------------------------------------
