41b02ceb262e7940a1331e9707048d2688a3b038
redrooster <devnull@localhost>
2010-05-21 22:54:10 +0200

    Encoding für Telefonnummern verbessert.

--- Commons/Telephone.py
+++ Commons/Telephone.py
@@ -3,8 +3,29 @@
 from pprint import pprint
 
 
-def encode_phonenumber(ctry_code, area_code, phone_number):
-    return "+%s (%s) %s" % (ctry_code, area_code, phone_number)
+def encode_phonenumber(ctry_code='', area_code='', phone_number='', 
+                       string_format='+%(ctry_code)s (%(area_code)s) %(phone_number)s'):
+    ''' string_format = +%(ctry_code)s (%(area_code)s) %(phone_number)s '''
+    
+    phone_number_dict = {}
+    
+    if ctry_code.startswith('00'): 
+        ctry_code = ctry_code[2:]
+    if ctry_code.startswith('+'):
+        ctry_code = ctry_code[1:]
+    if ctry_code <> '':
+        phone_number_dict['ctry_code'] = ctry_code
+        
+    if area_code.startswith('0'):
+        area_code = area_code[1:]
+    if area_code <> '':
+        phone_number_dict['area_code'] = area_code
+        
+    if phone_number <> '':
+        phone_number_dict['phone_number'] = phone_number
+        
+    phone_number_str = string_format % phone_number_dict
+    return phone_number_str
     
     
 def decode_phonenumber(phone_number):
@@ -56,5 +77,13 @@ if __name__ == "__main__":
     ctry_code, area_code, phone_number = decode_phonenumber(phone_number)
     print 'International: %s.\nVorwahl: %s.\nTelefon: %s.\n' % (ctry_code, area_code, phone_number)
     
+    ctry_code = raw_input('International: ')
+    area_code = raw_input('Vorwahl: ')
+    phone_number = raw_input('Telefonnummer: ')
+    
+    print
+    print encode_phonenumber(ctry_code, area_code, phone_number)
+    
     print; raw_input('... give <RETURN> to exit')
+    quit()
     
