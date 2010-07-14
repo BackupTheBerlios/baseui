f9065380cb2c89bcc59d240104e74c8ae851b2e3
redrooster <devnull@localhost>
2010-07-14 17:38:11 +0200

    Images für SVG fit gemacht.

--- wxApi/Images.py
+++ wxApi/Images.py
@@ -6,33 +6,24 @@
 #===============================================================================
 
 import wx
-#import wx.Frame
-
-# Here should be asked, if that components are installed!
 import wx.lib.wxcairo
+
 import rsvg
 import cairo
-
-class TestFrame(wx.Panel):
-    def __init__(self, parent, image_path): # , pos, size):
-        wx.Panel.__init__(self, parent, id = wx.ID_ANY) #, pos = wx.DefaultPosition, size = wx.Size( 200,460 ))
-        #wx.Frame.__init__(self, parent, id, title, pos, size)
-        
-        self.SVG = SVG(self, image_path)
-        self.Show()
-        
         
 
-class SVG:
+class SVG(wx.Panel):
     def __init__(self, parent, image_path): #, pos=(0,0), size=(1,1)):
-        self.parent = parent
+        wx.Panel.__init__(self, parent, id = wx.ID_ANY) #, pos = wx.DefaultPosition, size = wx.Size( 200,460 ))
+        
+        # self.parent = parent
         self.image_path = image_path
         
-        self.parent.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
         
         
     def OnPaint(self, event):
-        dc = wx.BufferedPaintDC(self.parent)
+        dc = wx.BufferedPaintDC(self)
         self.Render(dc)
         
 
@@ -51,8 +42,8 @@ class SVG:
         
 if __name__ == '__main__':
     app = wx.PySimpleApp()
-    frame = wx.Frame(None, id=wx.ID_ANY) #.__init__(None, id=wx.ID_ANY)
-    svg = TestFrame(frame) #, wx.ID_ANY )#, (200, 200), (800, 800))
+    frame = wx.Frame(None, id=wx.ID_ANY)
+    svg = SVG(frame, 'res/Auge.svg')
     frame.Show()
     app.MainLoop()
     
