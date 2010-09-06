82dccc224e1c02071258a5e02fbbd06759fd797d
redrooster <devnull@localhost>
2010-09-06 17:36:11 +0200

    Start- und Stop- Icon hinzugefügt, Webserver Toolbar eingeführt.

--- wxApi/Toolbars.py
+++ wxApi/Toolbars.py
@@ -11,6 +11,32 @@ from res import IconSet16
 from Buttons import BitmapTextToggleButton
 
 
+class WebserverToolbar(wx.ToolBar):
+    ID_START = 1
+    ID_STOP = 2
+    ID_PREFERENCES = 3
+    ID_HELP = 4
+    
+    def __init__(self, parent, preferences=True, help=True):
+        wx.ToolBar.__init__(self, parent, id=-1, pos=wx.DefaultPosition, 
+                            size=wx.DefaultSize, 
+                            style=wx.TB_FLAT | wx.TB_NODIVIDER)
+        self.SetToolBitmapSize(wx.Size(22, 22))
+        
+        self.AddLabelTool(id=self.ID_START, label="Start", bitmap=IconSet16.getstart_16Bitmap())
+        self.AddLabelTool(id=self.ID_STOP, label="Stop",  bitmap=IconSet16.getstop_16Bitmap())
+        
+        self.AddSeparator()
+        if preferences == True:
+            self.AddLabelTool(id=self.ID_PREFERENCES, label="Einstellungen", bitmap=IconSet16.getpreferences_16Bitmap())
+        
+        if help == True:
+            self.AddLabelTool(id=self.ID_HELP, label="Hilfe", bitmap=IconSet16.gethelp_16Bitmap())
+
+        self.Realize()
+        
+        
+                            
 class TableToolbar(wx.ToolBar):
     def __init__(self, parent, filter=True, search=True, preferences=True, help=True):
         wx.ToolBar.__init__(self, parent, id=-1, pos=wx.DefaultPosition, 
@@ -24,7 +50,7 @@ class TableToolbar(wx.ToolBar):
         self.AddLabelTool(id=-1, label=u"Löschen",   bitmap=IconSet16.getdelete_16Bitmap())
         
         self.AddSeparator()
-        self.AddLabelTool(id=-1, label="Drucken",       bitmap=IconSet16.getprint_16Bitmap())
+        self.AddLabelTool(id=-1, label="Drucken", bitmap=IconSet16.getprint_16Bitmap())
         
         if filter == True:
             self.AddSeparator()
@@ -45,7 +71,7 @@ class TableToolbar(wx.ToolBar):
             self.AddLabelTool(id=-1, label="Einstellungen", bitmap=IconSet16.getpreferences_16Bitmap())
         
         if help == True:
-            self.AddLabelTool(id=-1, label="Hilfe",         bitmap=IconSet16.gethelp_16Bitmap())
+            self.AddLabelTool(id=-1, label="Hilfe", bitmap=IconSet16.gethelp_16Bitmap())
         
         self.Realize()
         
@@ -63,7 +89,7 @@ class FormToolbar(wx.ToolBar):
         self.AddLabelTool(id=wx.ID_ANY, label="Löschen",    bitmap=IconSet16.getdelete_16Bitmap())
         
         self.AddSeparator()
-        self.AddLabelTool(id=wx.ID_ANY, label="Drucken",       bitmap=IconSet16.getprint_16Bitmap())
+        self.AddLabelTool(id=wx.ID_ANY, label="Drucken", bitmap=IconSet16.getprint_16Bitmap())
             
         if preferences == True or help == True:
             self.AddSeparator()
@@ -72,7 +98,7 @@ class FormToolbar(wx.ToolBar):
             self.AddLabelTool(id=-1, label="Einstellungen", bitmap=IconSet16.getpreferences_16Bitmap())
         
         if help == True:
-            self.AddLabelTool(id=-1, label="Hilfe",         bitmap=IconSet16.gethelp_16Bitmap())
+            self.AddLabelTool(id=-1, label="Hilfe", bitmap=IconSet16.gethelp_16Bitmap())
         
         self.Realize()
         
