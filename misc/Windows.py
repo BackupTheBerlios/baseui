a5476fe13a8d1d2e696331e428b3b41ced2dfc51
redrooster <devnull@localhost>
2010-10-12 13:22:30 +0200

    volume-serial-getter eingefügt.

--- misc/Windows.py
+++ misc/Windows.py
@@ -6,3 +6,13 @@
 #===============================================================================
 
 # For now, empty... but at least the drive serial getter comes here!
+
+import os
+
+
+def get_volume_serial(drive_letter):
+	out = os.popen('vol %s' % drive_letter)
+	out_str = out.read()
+	out_list = out_str.split(':')
+	drive_serial = out_list[3]
+	return drive_serial
