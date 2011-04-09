9c2fb740593e45183537f83aeb86e31dcb568667
redrooster <devnull@localhost>
2011-04-09 16:52:33 +0200

    Versionssprung auf 1.22, nach vielen Merges!

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # -*- coding: cp1252 -*-
-# This file initializes the GTKapi module. The __all__ list needs to
+# This file initializes the BaseUI module. The __all__ list needs to
 # contain all modules, which are accessed from outside of this module.
 # See Python tutorial (section packages) for more information.
 
 #__all__ = ['Portlets']
-__version__ = '1.21'
+__version__ = '1.22'
 __year__ = '2011'
 __author__ = 'Mark Muzenhardt'
 __appname__ = 'BaseUI'
