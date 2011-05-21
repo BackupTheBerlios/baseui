e4b869cddbd5dafb159781896c9abf064751ca29
redrooster <devnull@localhost>
2011-05-21 22:55:12 +0200

    MigrationTools.py weitergedacht.

--- dbApi/MigrationTools.py
+++ dbApi/MigrationTools.py
@@ -1,17 +1,28 @@
-'''
-Created on 21.05.2011
+# -*- coding: iso-8859-1 -*-
 
-@author: muzi
-'''
-
-class MyClass(object):
-    '''
-    classdocs
-    '''
+#===============================================================================
+# DBapi MigrationTools module.
+# by Mark Muzenhardt, published under LGPL-License.
+#===============================================================================
 
 
-    def __init__(selfparams):
-        '''
-        Constructor
-        '''
+def copy_columns(source_table, columns, target_table, id_column='id', distinct_column=None):
+    ''' Copys columns with content to another Table while looking up that there are
+        no doublettes at distinct_column. '''
         
+    pass
+
+
+def drop_columns(columns):
+    ''' Just drops a list of columns in one flow. '''
+    
+    pass
+
+
+def move_columns(source_table, columns, fk_column, target_table, id_column='id', distinct_column=None):
+    ''' Moves a list of columns to another table while looking up that there are
+        no doublettes at distinct_column. Creates a new field at the source table
+        which contains the foreign key to the new dataset in the target_table. '''
+
+    pass
+
