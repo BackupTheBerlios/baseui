f43e8697a79a41e704edf9a0cf1b74e89f8be243
redrooster <devnull@localhost>
2012-04-19 15:28:54 +0200

    XML bugfix, where instances have to be not longer packed to another object.

--- misc/XML.py
+++ misc/XML.py
@@ -2,49 +2,41 @@
 
 from pprint import pprint
 
-from xml.sax.handler import ContentHandler
-from xml.sax import make_parser
+#from xml.sax.handler import ContentHandler
+#from xml.sax import make_parser
 
 
 class XMLelement(object):
-    def __init__(self, tag=None, attributes=None, content=[]):
+    def __init__(self, tag=None, attributes=None, content=''):
         self.tag = tag
         self.attributes = attributes
         self.content = content
-        
-        if self.tag == None:
-            if isinstance(content, XMLelement):
-                self.tag = content.tag
-        
         self.level = 0
         
-        self.attributes_text = ''
-        if self.attributes <> None:
-            self.attributes_text = self.get_attributes_text(self.attributes)
+        if isinstance(content, XMLelement):
+            # If the content is an instance of an XMLelement, get tag, content 
+            # and attributes from that object below.
+            self.tag = content.tag
+            self.attributes = content.attributes
         
         
-    def add_child_dict(self, child):
-        self.content.append(child)
-        
-        
-    def get_child_dict(self, tag):
-        for tag_dict in self.content:
-            curr_tag = tag_dict.get('tag')
-            curr_content = tag_dict.get('content')
-            
-            #print curr_content
-            if curr_tag == None:
-                if type(curr_content) in [str, int]:
-                    curr_tag = curr_content
-                else:
-                    curr_tag = curr_content.tag
-                    #print 'MADE TO', curr_tag, 'EQUALS', tag
-            if curr_tag == tag:
-                return tag_dict
-        
-        
+    def get_child(self, tag=None):
+        for child_obj in self.content:
+            if tag == child_obj.tag:
+                return child_obj
+
+
+    def get_child_tags(self):
+        tag_list = []
+        for child_obj in self.content:
+            tag_list.append(child_obj.tag)
+        return tag_list
+    
+                
     def get_attributes_text(self, attributes):
-        #print attributes
+        if attributes == None:
+            return ''
+        
         text = ' '
         for key in attributes.keys():
             text += key + '="' + str(attributes.get(key)) + '" '
@@ -58,7 +50,7 @@ class XMLelement(object):
             s += '  '
         return s
 
-    
+        
     def generate_xml(self, level=0, depth=0, header=False):
         ''' Generates pretty xml for this object and all subobjects. The ``level``
             tells, which intendation level is needed, ``depth`` stands for the
@@ -70,80 +62,78 @@ class XMLelement(object):
             text += '<?xml version="1.0" encoding="utf-8"?>\n'
             
         self.level = level
-        text += '<%s%s>\n' % (self.tag, self.attributes_text)
+        text += '<%s%s>\n' % (self.tag, self.get_attributes_text(self.attributes))
         self.level += 1
         
-        for content_obj in self.content:
-            #print content_obj
-            tag = content_obj.tag
-            attributes = content_obj.attributes
-            content = content_obj.content
-            
-            if attributes == None:
-                attributes_text = ''
-            else:
-                attributes_text = self.get_attributes_text(attributes)
-            
-            start_tag = ''
-            end_tag = ''
-            if content == None:
-                content = ''
-
-            if isinstance(content, XMLelement):
-                content.level = self.level
-                content = content.generate_xml(level=self.level)
-            else:    
-                start_tag = '<%s%s>' % (tag, attributes_text)
-                end_tag = '</%s>' % tag
+        if type(self.content) == list:
+            # Content can be a list.
+            for element in self.content:
+                tag = element.tag
+                attributes = element.attributes
+                content = element.content
                 
-            text += '%s%s%s%s\n' % (self.spaces(), start_tag, content, end_tag)
-        self.level -= 1
-        text = text[:len(text)-1] + '\n' + '%s</%s>' % (self.spaces(), self.tag)
+                text += '%s<%s>\n' % (self.spaces(), tag)
+                if type(content) == list:
+                    self.level += 1
+                    for element in content:
+                        text += '%s%s' % (self.spaces(), element.generate_xml(level=self.level)) + '\n'
+                    self.level -= 1
+                    text += self.spaces()
+                else:
+                    text = text[:len(text)-1] + '%s' % content
+                text += '</%s>\n' % tag
+        else:
+            # Content can be a value.
+            text = text[:len(text)-1] + '%s' % self.content
+        
+        # Close the Tag at the end.
+        text += '</%s>' % self.tag
         return text
-        
-        
-        
-class XML_to_dict(ContentHandler):    
-    def __init__(self):
-        self.end_tag = False
 
         
-    def startDocument(self):
-        self.dict_str = ""
-
-        
-    def endDocument(self):
-        self.dict_str = self.dict_str[0:len(self.dict_str)-1]
-        self.dict_str += '}'
-        self.dict = eval(self.dict_str)
-        
-
-    def startElement(self, name, attrs):
-        self.name = name
-        if self.end_tag:
-            self.end_tag = False
-            self.dict_str += "'%s':" % name
-        else:
-            self.dict_str += "{'%s':" % name
         
         
-    def endElement(self, name):
-        if name == self.name:
-            if self.dict_str[len(self.dict_str)-1:len(self.dict_str)] == ":":
-                self.dict_str += "''"
-            self.dict_str += ","
-            self.end_tag = True
-        else:
-            self.dict_str = self.dict_str[0:len(self.dict_str)-1]
-            self.dict_str += "},"
-        print 'end:', name
-
-    
-    def characters(self, content):
-        content = content.replace('\t', '').replace('\n', '')
-        if content <> '':
-            self.dict_str += "'%s'" % content
-            print 'content:', content,
+#class XML_to_dict(ContentHandler):    
+#    def __init__(self):
+#        self.end_tag = False
+#
+#        
+#    def startDocument(self):
+#        self.dict_str = ""
+#
+#        
+#    def endDocument(self):
+#        self.dict_str = self.dict_str[0:len(self.dict_str)-1]
+#        self.dict_str += '}'
+#        self.dict = eval(self.dict_str)
+#        
+#
+#    def startElement(self, name, attrs):
+#        self.name = name
+#        if self.end_tag:
+#            self.end_tag = False
+#            self.dict_str += "'%s':" % name
+#        else:
+#            self.dict_str += "{'%s':" % name
+#        
+#        
+#    def endElement(self, name):
+#        if name == self.name:
+#            if self.dict_str[len(self.dict_str)-1:len(self.dict_str)] == ":":
+#                self.dict_str += "''"
+#            self.dict_str += ","
+#            self.end_tag = True
+#        else:
+#            self.dict_str = self.dict_str[0:len(self.dict_str)-1]
+#            self.dict_str += "},"
+#        print 'end:', name
+#
+#    
+#    def characters(self, content):
+#        content = content.replace('\t', '').replace('\n', '')
+#        if content <> '':
+#            self.dict_str += "'%s'" % content
+#            print 'content:', content,
 
 
 
