b7f322424d541a1fa809a9ac56404969594a5f9a
redrooster <devnull@localhost>
2012-04-21 10:48:07 +0200

    Anpassung der npp.bat auf Python2.7

--- dev/npp.bat
+++ dev/npp.bat
@@ -5,7 +5,7 @@ set rootdir=%1
 %rootdir:~1, +2%
 cd %1 
 
-C:\Python26\python.exe %2
+C:\Python27\python.exe %2
 
 REM This makes notepad++ work together with python. Let npp execute that command:
 REM "npp.bat" "$(CURRENT_DIRECTORY)" "$(FILE_NAME)"
