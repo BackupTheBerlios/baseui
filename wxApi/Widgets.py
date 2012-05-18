ed3ddd61237e5f05da0cf5e6c6c2ac087df70ea7
redrooster <devnull@localhost>
2012-05-18 03:53:29 +0200

    TrayIcon funktioniert jetzt so wie es soll.

--- wxApi/Widgets.py
+++ wxApi/Widgets.py
@@ -59,57 +59,31 @@ class BufferedWindow(wx.ScrolledWindow):
             
 
 class TrayIcon(wx.TaskBarIcon):
-    def __init__(self, frame, icon):
+    def __init__(self, frame, icon, tooltip=''):
         wx.TaskBarIcon.__init__(self)
         
         self.frame = frame
         self.icon = icon
+        self.tooltip = tooltip
         
-        # self.frame.Bind(wx.EVT_SIZE, self.on_size)
+        self.Bind(wx.EVT_TASKBAR_LEFT_UP, self.on_left_up)
         self.frame.Bind(wx.EVT_ICONIZE, self.on_minimize)
-        self.frame.Bind(wx.EVT_CLOSE, self.on_close)
         
-        self.create_menu()
-        self.Bind(wx.EVT_MENU, self.on_resume, id=wx.ID_EXIT)
-        self.Bind(wx.EVT_MENU, self.on_resume, id=101)
         
-
-    def create_menu(self):
-        self.Bind(wx.EVT_TASKBAR_RIGHT_UP, self.show_menu)
- 
-        self.menu = wx.Menu()
-        self.menu.Append(101, u'&Wiederherstellen')
-        self.menu.AppendSeparator()
-        self.menu.Append(wx.ID_EXIT, u'&Schlieﬂen')
+    def on_left_up(self, event):
+        if self.frame.IsIconized():
+            self.frame.Iconize(False)
+            self.frame.Show()
+            self.frame.Restore()
+            self.RemoveIcon()
+            
         
-
-    def show_menu(self, event):
-        self.PopupMenu(self.menu)
-
-        
-    def on_resume(self, event):
-        self.RemoveIcon()
-        self.frame.Iconize(False)
-        #self.frame.Show(True)
-        
- 
-    def on_close(self, event):
-        self.RemoveIcon()
-        self.Destroy()
-        event.Skip()
- 
- 
     def on_minimize(self, event):
-        self.SetIcon(self.icon)
-        self.frame.Iconize(True)
-        #self.frame.Show(False)
- 
- 
-    # def on_size(self, event):   
-    #     size = self.frame.GetClientSize()
-    #     self.frame.text.SetSize(size)
-    #     self.frame.panel.SetSize(size)
-    #     event.Skip()
+        if event.Iconized():
+            self.frame.Hide()
+            self.frame.Iconize(True)
+            self.SetIcon(self.icon, self.tooltip)
+            
         
         
 def init_widgets(definition_dict):
