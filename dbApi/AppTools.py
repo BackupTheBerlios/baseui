f4994f63286b9ecf804a87d70091ea7b2af33ae7
redrooster <devnull@localhost>
2012-06-15 15:35:31 +0200

    AppTools.IniTable.get_section erweitert.

--- dbApi/AppTools.py
+++ dbApi/AppTools.py
@@ -36,25 +36,47 @@ class IniTable(SQLdb.table):
             value = result[0].get('_value')
         return value
         
-    
+        
     def set_option(self, section, option, value):
         result = self.select(where="_section = '%s' AND _option = '%s'" % (section, option))
         if result <> []:
             self.update({'_value': value}, where="_section = '%s' AND _option = '%s'" % (section, option))
         else:
             self.insert('id', {'_section': section, '_option': option, '_value': value})
-    
-    
-    def get_section(self, section, options_dict):
+            
+            
+    def get_section(self, section, options_dict=None, prefix_section=False):
+        ''' Returns a dict containing all options as key and all values as value.
+            if options_dict is given, just the containing keys are fetched. If
+            the keys are not already in the section, it adds them with the given
+            default (if there is any). If prefix_section is True, all keys will
+            be prefixed with the section name + underscore (<section>_<option>).'''
+        
         section_dict = {}
         
+        # If no options_dict is given,simply read all options in the given section.
+        if options_dict == None:
+            result = self.select(where="_section = '%s'" % section)
+            for row in result:
+                option = row.get('_option')
+                if option == None:
+                    continue
+                if prefix_section == True:
+                    option = '%s_%s' % (section, option)
+                value = row.get('_value')
+                section_dict[option] = value
+            return section_dict
+            
+        # If options_dict is given for default, return just its keys in a new dict.
         for option in options_dict:
             value = self.get_option(section, option, options_dict[option])
+            if prefix_section == True:
+                option = '%s_%s' % (section, option)
             section_dict[option] = value
         return section_dict
         
         
-    def save_lod(self, content_lod):
+    def save_lod(self, content_lod, remove_prefix=False):
         for content_dict in content_lod:
             section = content_dict.get('section')
             option = content_dict.get('option')
