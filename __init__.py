c5e5d043d6253552e25a06929aa295d096bc74bb
redrooster <devnull@localhost>
2012-11-12 11:57:21 +0100

    Version 2.18

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1252 -*-
 
-__version__ = '3.0'
+__version__ = '2.18'
 __year__ = '2012'
 __author__ = 'Mark Muzenhardt'
 __appname__ = 'BaseUI'
-__license__ = 'BSD'
+__license__ = 'LGPL'
